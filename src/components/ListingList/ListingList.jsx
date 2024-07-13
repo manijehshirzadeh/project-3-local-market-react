8ad292ca6dd00fe82f83d87e5de6fd0277981cae
parisa-naeim-ga <parisa.naeim@gmail.com>
2024-07-14 00:34:55 +1000

    improve css

--- src/components/ListingList/ListingList.jsx
+++ src/components/ListingList/ListingList.jsx
@@ -1,25 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const ListingList = (props) => {
-    const listingListItems = props.listings.map((listing) => (
-        <Link key={ listing._id } to={"/listings/" + listing._id}>
-            <article>
-                <header>
-                    <h2>{ listing.title }</h2>
-                    <p>
-                        { listing.seller.username } posted on { new Date(listing.createdAt).toLocaleDateString() }
-                    </p>
-                </header>
-                <p>{ listing.text }</p>
-            </article>
-        </Link>
-    ));
+  const listingListItems = props.listings.map((listing) => (
+    <Link key={listing._id} to={"/listings/" + listing._id}>
+      <article className="listing">
+        <header>
+          <h2>{listing.title}</h2>
+          <p>
+            {listing.seller.username} posted on{" "}
+            {new Date(listing.createdAt).toLocaleDateString()}
+          </p>
+        </header>
+        <p>{listing.text}</p>
+      </article>
+    </Link>
+  ));
 
-    return (
-        <main>
-            { listingListItems }
-        </main>
-    );
+  return <main className="listing-container">{listingListItems}</main>;
 };
 
 export default ListingList;
