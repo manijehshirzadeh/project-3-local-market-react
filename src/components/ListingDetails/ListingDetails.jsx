7268fc03a9df436449a7763190859a10698bddbf
parisa-naeim-ga <parisa.naeim@gmail.com>
2024-07-17 00:47:02 +1000

    add map to listing details

--- src/components/ListingDetails/ListingDetails.jsx
+++ src/components/ListingDetails/ListingDetails.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useContext } from "react";
 import { Link, useParams } from "react-router-dom";
 import { AuthedUserContext } from "../App";
 import * as listingService from "../../services/listingService";
+import MapComponent from "../MapComponent/MapComponent";
 
 const ListingDetails = (props) => {
   const [listing, setListing] = useState(null);
@@ -46,102 +47,121 @@ const ListingDetails = (props) => {
   return (
     <main>
       <header>
-        <p className="lead" style={{ textTransform: "uppercase" }}>
+        <p
+          className="lead"
+          style={{ textTransform: "uppercase", textDecoration: "underline" }}
+        >
           {listing.category}
         </p>
       </header>
 
       <section>
-        <div
-          id="myModal"
-          className="modal-div"
-          style={{ display: showBidForm ? "block" : "none" }}
-        >
-          <div className="modal-content-div">
-            <span
-              className="modal-close-btn"
-              onClick={() => setShowBidForm(false)}
-            >
-              &times;
-            </span>
-            <div>
-              <label htmlFor="price-input" className="form-label">
-                Price
-              </label>
+        <div>
+          <div
+            id="myModal"
+            className="modal-div"
+            style={{ display: showBidForm ? "block" : "none" }}
+          >
+            <div className="modal-content-div">
+              <span
+                className="modal-close-btn"
+                onClick={() => setShowBidForm(false)}
+              >
+                &times;
+              </span>
+              <div>
+                <label htmlFor="price-input" className="form-label">
+                  Price
+                </label>
 
-              <input
-                className="form-control me-2 w-50 d-inline mx-4"
-                type="number"
-                name="price"
-                id="price-input"
-                value={bidFormData.price}
-                onChange={onBidFormChange}
-                min={1}
-              ></input>
-              <div style={{ textAlign: "center" }}>
-                <button
-                  className="btn btn-primary btn-m mx-3 px-5 py-2 mt-4"
-                  onClick={submitBid}
-                >
-                  Submit bid
-                </button>
+                <input
+                  className="form-control me-2 w-50 d-inline mx-4"
+                  type="number"
+                  name="price"
+                  id="price-input"
+                  value={bidFormData.price}
+                  onChange={onBidFormChange}
+                  min={1}
+                ></input>
+                <div style={{ textAlign: "center" }}>
+                  <button
+                    className="btn btn-primary btn-m mx-3 px-5 py-2 mt-4"
+                    onClick={submitBid}
+                  >
+                    Submit bid
+                  </button>
+                </div>
               </div>
             </div>
           </div>
+
+          <img src="https://placehold.co/400x300.png" />
+          <h2 className="display-6 py-2">{listing.title}</h2>
+          <h4 className="h4">AU${listing.price}</h4>
+          <p style={{ color: "#aaaaaa" }}>
+            Listed on {new Date(listing.createdAt).toLocaleDateString()} by{" "}
+            {listing.seller.username}
+          </p>
+
+          <div
+            className="d-flex gap-5 mt-5"
+            style={{ minWidth: "800px", maxWidth: "50%" }}
+          >
+            <div>
+              <p>Description:</p>
+              <p>{listing.description}</p>
+              <p>Condition: {listing.condition}</p>
+              <h2>Bids</h2>
+
+              {listing.bids.length === 0 && <p>No bids yet.</p>}
+
+              {listing.bids.map((bid) => (
+                <article key={bid._id}>
+                  <header>
+                    <p>
+                      AU${bid.price} bid made by {bid.bidder.username} on{" "}
+                      {new Date(bid.createdAt).toLocaleDateString()}
+                    </p>
+                  </header>
+                </article>
+              ))}
+
+              {listing.seller._id === user._id && (
+                <>
+                  <Link
+                    className="btn btn-primary btn-m px-5 py-2 mt-2"
+                    to={`/listings/${listing._id}/edit`}
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    className="btn btn-primary btn-m mx-3 px-5 py-2 mt-2"
+                    onClick={() => props.handleDeleteListing(listing._id)}
+                  >
+                    Delete
+                  </button>
+                </>
+              )}
+
+              {listing.seller._id !== user._id && (
+                <>
+                  <button
+                    className="btn btn-primary btn-m mx-3 px-5 py-2 mt-2"
+                    onClick={() => setShowBidForm(true)}
+                    type="button"
+                  >
+                    Make a bid
+                  </button>
+                </>
+              )}
+            </div>
+
+            <div>
+              <p>Pick up postcode: {listing.postcode}</p>
+              <MapComponent postcode={listing.postcode} />
+            </div>
+          </div>
         </div>
-
-        <img src="https://placehold.co/400x300.png" />
-        <h2 className="display-4">{listing.title}</h2>
-        <h4 className="h4">AU${listing.price}</h4>
-        <p style={{ color: "#aaaaaa" }}>
-          Listed on {new Date(listing.createdAt).toLocaleDateString()} by{" "}
-          {listing.seller.username}
-        </p>
-        <p>{listing.description}</p>
-        <p>Condition: {listing.condition}</p>
-        <p>Pick up: {listing.postcode}</p>
-        <h2>Bids</h2>
-
-        {listing.bids.length === 0 && <p>No bids yet.</p>}
-        {listing.bids.map((bid) => (
-          <article key={bid._id}>
-            <header>
-              <p>
-                AU${bid.price} bid made by {bid.bidder.username} on{" "}
-                {new Date(bid.createdAt).toLocaleDateString()}
-              </p>
-            </header>
-          </article>
-        ))}
-
-        {listing.seller._id === user._id && (
-          <>
-            <Link
-              className="btn btn-primary btn-m px-5 py-2 mt-2"
-              to={`/listings/${listing._id}/edit`}
-            >
-              Edit
-            </Link>
-            <button
-              className="btn btn-primary btn-m mx-3 px-5 py-2 mt-2"
-              onClick={() => props.handleDeleteListing(listing._id)}
-            >
-              Delete
-            </button>
-          </>
-        )}
-
-        {listing.seller._id !== user._id && (
-          <>
-            <button
-              className="btn btn-primary btn-m mx-3 px-5 py-2 mt-2"
-              onClick={() => setShowBidForm(true)}
-              type="button"
-            >
-              Make a bid
-            </button>
-          </>
-        )}
       </section>
     </main>
   );
